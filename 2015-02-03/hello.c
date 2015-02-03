103b15343749cb1d048fd99ba2383ebc0b2abc33
sunshine <17372948908ltt@163.com>
2015-02-03 21:52:34 +0800

    c

---
+++ 2015-02-03/hello.c
@@ -0,0 +1,35 @@
+// Last Update:2015-02-03 21:39:46
+ /*
+#include <stdio.h>
+int main()
+{
+    printf("你是我的小啊小苹果儿\r");
+    printf("怎么爱你都不嫌\r");
+    printf("红红的小脸儿温暖我的心窝\n");
+    printf("点亮我生命的火，活活活活哦\n");
+    return 0;
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+    //这是一首诗
+    printf("床前明月光，");
+    printf("疑是地上霜。");
+    printf("举头望明月,");
+    printf("低头思故乡。");
+    return 0;
+}
+*/
+
+#include<stdio.h> 
+int main()
+{
+    int age;
+    age = 100;
+    printf("我今年%d岁\n", age);
+    return 0;
+}
+
