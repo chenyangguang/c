de19e62c5d848459c6bb6b5c5d20e899e77b2405
sunshine <17372948908ltt@163.com>
2015-02-17 07:38:43 +0800

    basic, array

---
+++ 2015-02-16/getch.c
@@ -0,0 +1,8 @@
+// Last Update:2015-02-16 22:46:01
+#include <stdio.h>
+int main()
+{
+    printf("zixue\n");
+    //getch();  //wait input
+    return 0; //exit
+}
---
+++ 2015-02-17/zixue7.c
@@ -0,0 +1,21 @@
+// Last Update:2015-02-17 07:35:18
+#include <stdio.h>
+int main()
+{
+    unsigned int i = 0, k = -5; //unsigned
+    short n = 2;
+    long j = 10;
+    char ch = 'a'; //'', ""
+    double db = 5.5;
+    float fd = 15.5; 
+
+    char a[] = "chenyangguang"; //array
+
+    //k = 10;
+    //printf("%d\n", k);
+    //printf("%.1f\n", db); //
+    printf("%c\n", ch);
+    //scanf("%d", &k); //& address
+    printf("%u\n", k);
+    return 0;
+}
