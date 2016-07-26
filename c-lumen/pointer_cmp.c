03f04fe87714da7be3d8155571e79ec8535f9459
chenyangguang <vim.09071@gmail.com>
2016-07-26 08:11:26 +0800

    * pointer compare

---
+++ c-lumen/pointer_cmp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int * p1, * p2, *p, a, b;
+    printf("please enter two integer numbers:");
+    scanf("%d, %d", &a, &b);
+    p1 = &a;
+    p2 = &b;
+    if (a < b) {
+        p = p1;
+        p1 = p2;
+        p2 = p;
+    }
+    printf("a=%d, b=%d\n", a, b);
+    printf("max=%d, min=%d\n", b, a);
+
+    return 0;
+}
