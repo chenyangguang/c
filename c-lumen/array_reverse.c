bd0cdec0e0dbb3ef31e70585fccfcedcd82f8a7a
chenyangguang <vim.09071@gmail.com>
2016-07-26 08:53:02 +0800

    * array_reverse

---
+++ c-lumen/array_reverse.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    // 数组元素反转
+    void inverse(int x[], int n);
+    int i, a[] = {3, 7, 9, 11, 0, 6, 7, 5, 4, 2};
+    printf("The original array:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d\t", a[i]);
+    }
+    printf("\n");
+    inverse(a, 10);
+    printf("The array has been inverted:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d\t", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void inverse(int x[], int n)
+{
+    int temp, i, j, m = (n - 1) / 2;
+    for (i = 0; i <= m; i++) {
+        j = n - 1 - i;
+        temp = x[i];
+        x[i] = x[j];
+        x[j] = temp;
+    }
+    return ;
+}
+
