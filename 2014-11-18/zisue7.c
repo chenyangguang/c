4f75b69e1db001a8e63c05df204ac87f4765d377
chenyangguang <vim.09071@gmail.com>
2016-09-13 16:00:26 +0800

    * delete unimportant test

--- 2014-11-18/zisue7.c
+++
@@ -1,9 +0,0 @@
-// Last Update:2014-11-18 22:58:46
-#include <stdio.h>
-int main()
-{
-    char a[] = "zixue\0.cont";
-    printf("%s", a);
-    return 0;
-}
-
--- 2015-02-16/getch.c
+++
@@ -1,8 +0,0 @@
-// Last Update:2015-02-16 22:46:01
-#include <stdio.h>
-int main()
-{
-    printf("zixue\n");
-    //getch();  //wait input
-    return 0; //exit
-}
