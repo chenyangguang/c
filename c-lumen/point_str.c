012eeb7479741ecde3770a9269618fb18c105284
chenyangguang <vim.09071@gmail.com>
2016-09-13 15:44:37 +0800

    * update

--- c-lumen/point_str.c
+++ c-lumen/point_str.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+
 int main()
 {
-    char * string = "Test String";
+    char *string = "Test String";
+    int length = strlen(string), i;
+
     printf("%s\n", string);
+
+    // use *(string+i)
+    for(i=0; i<length; i++){
+      printf("%c", *(string+i));
+    }
+    printf("\n");
+
+    // use string[i]
+    for(i=0; i<length; i++){
+      printf("%c", string[i]);
+    }
 
     return 0;
 }
