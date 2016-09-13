8adc0c779fd19ed9d465b096da703177b442e55b
chenyangguang <vim.09071@gmail.com>
2016-09-13 16:22:23 +0800

    * pointer 是特殊的类型

--- c-lumen/pointer_type.c
+++ c-lumen/pointer_type.c
@@ -6,20 +6,20 @@ int main()
   double b = 99.9, *pb = &b;
   char c = '@', *pc = &c;
 
-  printf("&a=%X, &b=%X, &c=%X\n", &a, &b, &c);
-  printf("pa=%X, pb=%X, pc=%X\n", pa, pb, pc);
+  printf("&a=%p, &b=%p, &c=%p\n", &a, &b, &c);
+  printf("pa=%p, pb=%p, pc=%p\n", pa, pb, pc);
 
   pa++;
   pb++;
   pc++;
-  printf("pa=%#X, pb=%#X, pc=%#X\n", pa, pb, pc);
+  printf("pa=%p, pb=%p, pc=%p\n", pa, pb, pc);
 
   pa -= 2;
   pb -= 2;
   pc -= 2;
-  printf("pa=%#X, pb=%#X, pc=%#X\n", pa, pb, pc);
+  printf("pa=%p, pb=%p, pc=%p\n", pa, pb, pc);
 
-  if(pa === paa){
+  if(pa == paa){
     printf("%d\n", *paa);
   }else{
     printf("%d\n", *pa);
