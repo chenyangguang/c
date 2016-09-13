6b4b47b7cbcc3da6322e646b275759e1fed2ee4e
chenyangguang <vim.09071@gmail.com>
2016-09-13 15:26:24 +0800

    * test

--- c-lumen/pointer_type.c
+++ c-lumen/pointer_type.c
@@ -6,8 +6,24 @@ int main()
   double b = 99.9, *pb = &b;
   char c = '@', *pc = &c;
 
-  printf("&a=%#X, &b=%#X, &c=%#X\n", &a, &b, &c);
+  printf("&a=%X, &b=%X, &c=%X\n", &a, &b, &c);
+  printf("pa=%X, pb=%X, pc=%X\n", pa, pb, pc);
+
+  pa++;
+  pb++;
+  pc++;
   printf("pa=%#X, pb=%#X, pc=%#X\n", pa, pb, pc);
+
+  pa -= 2;
+  pb -= 2;
+  pc -= 2;
+  printf("pa=%#X, pb=%#X, pc=%#X\n", pa, pb, pc);
+
+  if(pa === paa){
+    printf("%d\n", *paa);
+  }else{
+    printf("%d\n", *pa);
+  }
 
   return 0;
 }
