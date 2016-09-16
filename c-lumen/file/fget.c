0e411543ba9b021b3c7b4e0abf73f49a006e08c1
nil <vim.09071@gmail.com>
2016-09-16 09:52:46 +0800

    * 向文件读写字符

---
+++ c-lumen/file/fget.c
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fget.c
+ *
+ *    Description:  从键盘输入一些字符，逐个把它们送到磁盘，知道用户输入一个“#”号结束
+ *
+ *        Version:  1.0
+ *        Created:  09/16/2016 09:39:15 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (chenyangguang), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    FILE *fp;
+    char ch,filename[10];
+    printf("Please input a name of file:");
+    scanf("%s", filename);
+
+    if(fp=fopen(filename, "w") == NULL)
+    {
+        printf("Can't open the file\n");
+        exit(0);
+    }
+    ch = getchar();
+    printf("Please enter a string(end with #): ");
+    ch = getchar();
+    while(ch != '#')
+    {
+        fputc(ch, fp);
+        putchar(ch);
+        ch = getchar();
+    }
+    fclose(fp);
+    putchar(10);
+
+    return 0;
+}
